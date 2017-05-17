9282c1ffe5e0407e6b65f993a92cd794b399ab32
Arthur Puthin <arthur.rprp@gmail.com>
2017-05-17 01:13:22 -0300

    Folder Structure & Initial Commit

---
+++ server.js
@@ -0,0 +1,2 @@
+// TODO ADD SOURCEMAP SUPPORT
+require("./build/server");
---
+++ src/server/server.js
@@ -0,0 +1,6 @@
+const bleh = 'wow';
+console.log(`hello ${bleh}`);
+
+const obj = {her: 1};
+const obj2 = {...obj, blegh: 2};
+console.log(obj2);

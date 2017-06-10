06d23028119817f1675f398640631dba388112b0
Arthur Puthin <arthur.rprp@gmail.com>
2017-06-10 19:40:31 -0300

    Extra package setup for client development

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ var path = require('path'),
 const vendor = [
   'lodash',
   'react',
-  'react-dom'
+  'react-dom',
+  'react-router',
+  'socket.io-client',
+  'rxjs'
 ]
 
 function createConfig(isDebug) {
@@ -33,7 +36,7 @@ function createConfig(isDebug) {
     files: { test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: 'url-loader?limit=5000'}
   }
 
-  const clientEntry = ['./src/client/client.js']
+  const clientEntry = ['babel-polyfill', './src/client/client.js']
   let publicPath = '/build/'
 
   if (isDebug) {

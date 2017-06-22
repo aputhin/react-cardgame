c56ea1ccaf9ecb0ad1ab9f2b4aead3d0173bad6d
Arthur Puthin <arthur.rprp@gmail.com>
2017-06-21 23:25:42 -0300

    Game constants, actions e subscriptions (full store)

--- src/client/stores/game.js
+++ src/client/stores/game.js
@@ -1,17 +1,52 @@
+import {Observable, BehaviorSubject} from 'rxjs'
 import {mapOp$} from 'shared/observable'
 import * as A from '../actions'
+import _ from 'lodash'
+
+const defaultView = {
+  id: 42,
+  title: 'I haz game',
+  step: A.STEP_SETUP,
+  options: {
+    scoreLimit: 5,
+    sets: ['1ed']
+  },
+  players: [
+    {id: 1, name: 'arthur', score: 0, isCzar: false, isPlaying: false, isWinner: false},
+    {id: 2, name: 'carlos', score: 0, isCzar: false, isPlaying: false, isWinner: false},
+    {id: 3, name: 'josué', score: 0, isCzar: false, isPlaying: false, isWinner: false},
+    {id: 4, name: 'miguel', score: 0, isCzar: false, isPlaying: false, isWinner: false},
+    {id: 5, name: 'cecília', score: 0, isCzar: false, isPlaying: false, isWinner: false}
+  ],
+  round: null,
+  timer: null
+}
+
+const defaultPlayerView = {
+  id: 1,
+  hand: [],
+  stack: null
+}
 
 export default class GameStore {
   constructor({dispatcher}, user) {
-    const isLoggedIn$ = user.details$.map(d => d.isLoggedIn)
-
     dispatcher.onRequest({
       [A.GAME_CREATE]: action => {
         dispatcher.succeed(action)
         dispatcher.succeed(A.gameJoin(42))
       },
-      [A.GAME_JOIN]: action => dispatcher.succeed(action)
+      [A.GAME_JOIN]: action => dispatcher.succeed(action),
+      [A.GAME_SET_OPTIONS]: action => dispatcher.succeed(action),
+      [A.GAME_START]: action => dispatcher.succeed(action),
+      [A.GAME_SELECT_CARD]: action => dispatcher.succeed(action),
+      [A.GAME_SELECT_STACK]: action => dispatcher.succeed(action),
+      [A.GAME_SEND_MESSAGE]: action => dispatcher.succeed(action)
     })
+
+    this.view$ = new BehaviorSubject(defaultView)
+    this.player$ = new BehaviorSubject(defaultPlayerView)
+
+    const isLoggedIn$ = user.details$.map(d => d.isLoggedIn)
 
     this.opCreateGame$ = mapOp$(
       dispatcher.on$(A.GAME_CREATE),
@@ -20,6 +55,44 @@ export default class GameStore {
 
     this.opJoinGame$ = mapOp$(
       dispatcher.on$(A.GAME_JOIN)
+    )
+
+    this.opSetOptions$ = mapOp$(
+      dispatcher.on$(A.GAME_SET_OPTIONS),
+      isLoggedIn$
+    )
+
+    this.opStart$ = mapOp$(
+      dispatcher.on$(A.GAME_START),
+      isLoggedIn$
+    )
+
+    this.opStart$ = mapOp$(
+      dispatcher.on$(A.GAME_START),
+      isLoggedIn$
+    )
+
+    const playerAndGame$ = Observable.combineLatest(this.view$, this.player$)
+
+    this.opSelectCard$ = mapOp$(
+      dispatcher.on$(A.GAME_SELECT_CARD),
+      playerAndGame$.map(([game, player]) => {
+        const ourPlayer = _.find(game.players, {id: player.id})
+        return ourPlayer && game.step === A.STEP_CHOOSE_WHITES && ourPlayer.isPlaying
+      })
+    )
+
+    this.opSelectStack$ = mapOp$(
+      dispatcher.on$(A.GAME_SELECT_STACK),
+      playerAndGame$.map(([game, player]) => {
+        const ourPlayer = _.find(game.players, {id: player.id})
+        return ourPlayer && game.step === A.STEP_JUDGE_STACKS && ourPlayer.isCzar
+      })
+    )
+
+    this.opSendMessage$ = mapOp$(
+      dispatcher.on$(A.GAME_SEND_MESSAGE),
+      isLoggedIn$
     )
   }
 }
--- src/server/shared/actions.js
+++ src/server/shared/actions.js
@@ -43,3 +43,35 @@ export const gameCreate = () => ({type: GAME_CREATE})
 
 export const GAME_JOIN = 'GAME_JOIN'
 export const gameJoin = (gameId) => ({type: GAME_JOIN, gameId})
+
+export const GAME_SET_OPTIONS = 'GAME_SET_OPTIONS'
+export const gameSetOptions = (gameId, options) => ({type: GAME_SET_OPTIONS, gameId, options})
+
+export const GAME_START = 'GAME_START'
+export const gameStart = (gameId) => ({type: GAME_START, gameId})
+
+export const GAME_SELECT_CARD = 'GAME_SELECT_CARD'
+export const gameSelectCard = (gameId, cardId) => ({type: GAME_SELECT_CARD, gameId, cardId})
+
+export const GAME_SELECT_STACK = 'GAME_SELECT_STACK'
+export const gameSelectStack = (gameId, stackId) => ({type: GAME_SELECT_STACK, gameId, stackId})
+
+export const GAME_SEND_MESSAGE = 'GAME_SEND_MESSAGE'
+export const gameSendMessage = (gameId, message) => ({type: GAME_SEND_MESSAGE, gameId, message})
+
+/**
+ * Game Constants
+ */
+export const STEP_SETUP = 'STEP_SETUP'
+export const STEP_CHOOSE_WHITES = 'STEP_CHOOSE_WHITES'
+export const STEP_JUDGE_STACKS = 'STEP_JUDGE_STACKS'
+export const STEP_WAIT = 'STEP_WAIT'
+export const STEP_DISPOSED = 'STEP_DISPOSED'
+
+export const WAIT_GAME_OVER = 'WAIT_GAME_OVER'
+export const WAIT_ROUND_OVER = 'WAIT_ROUND_OVER'
+
+export const WAIT_REASON_GAME_FINISHED = 'WAIT_REASON_GAME_FINISHED'
+export const WAIT_REASON_TOO_FEW_PLAYERS = 'WAIT_REASON_TOO_FEW_PLAYERS'
+export const WAIT_REASON_CZAR_LEFT = 'WAIT_REASON_CZAR_LEFT'
+export const WAIT_REASON_ROUND_FINISHED = 'WAIT_REASON_ROUND_FINISHED'

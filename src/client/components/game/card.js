b6b19e99aa167998d0c221b57959c126109b9f78
Arthur Puthin <arthur.rprp@gmail.com>
2017-06-29 21:34:05 -0300

    Card component and styling

--- src/client/components/game/card.js
+++ src/client/components/game/card.js
@@ -4,6 +4,31 @@ import React from 'react'
 
 export default function Card(props) {
   const {isSelectable = false, onClick, type, card, style = 'default', canZoom = false} = props
+  const classes = [
+    'c-card',
+    type,
+    card ? 'front' : 'back',
+    `style-${style}`,
+    canZoom ? 'can-zoom' : '',
+    isSelectable ? 'is-selectable': ''
+  ]
 
-  return <p>CARD STUB</p>
+  const click = () => {
+    isSelectable && onClick(card)
+  }
+
+  return (
+    <div className={classes.join(' ')} onDoubleClick={click}>
+      {!card
+        ? <div className="inner">Cards</div> :
+        <div className="inner">
+          <span className="text">{card.text}</span>
+          <span className="set">{card.set}</span>
+          {!card.whiteCardCount ? null : 
+            <span className="white-count">
+              Pick <span>{card.whiteCardCount}</span>
+            </span>}
+        </div>}
+    </div>
+  )
 }

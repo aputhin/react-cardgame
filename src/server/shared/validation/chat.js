ae2ce48c3c3f4aab9cd752ee83aebf84b32a689e
Arthur Puthin <arthur.rprp@gmail.com>
2018-02-01 00:20:57 -0200

    Adds diffs with manually made splices 🙄

--- src/server/shared/validation/chat.js
+++ src/server/shared/validation/chat.js
@@ -3,7 +3,7 @@ import {Validator} from './index'
 export const MESSAGE_LENGTH_LIMIT = 100
 export function validateMessage(message) {
   if (message.length > MESSAGE_LENGTH_LIMIT) {
-    return Validator.fail(`Messages must be fewe than ${MESSAGE_LENGTH_LIMIT} chars long`)
+    return Validator.fail(`Messages must be fewer than ${MESSAGE_LENGTH_LIMIT} chars long`)
   }
 
   return Validator.succeed()
